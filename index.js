0c7afde18b730ca32aac42a20325167a733433e0
NPitroff <nicolai.pitroff@gmail.com>
2022-01-27 07:18:36 -0800

    Added an if statement and changed the selectors to alter the dice image

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // Variable to select player 1 dice
-var playerOne = document.getElementById("playerOne");
+var playerOne = document.getElementsByClassName("img1");
 // variable to select player 2 dice
-var playerTwo = document.getElementById("playerTwo");
+var playerTwo = document.getElementsByClassName("img2");
 
 
 
-randomNumber1 = Math.floor(Math.random()*6)+1;
+//randomNumber1 = Math.floor(Math.random()*6)+1;
+// loop for player 1 dice to switch the img src
+randomNumber1 = 1;
 console.log(randomNumber1);
+
+if(randomNumber1 === 1){
+  playerOne[0].setAttribute("src", "images/dice1.png");
+}
