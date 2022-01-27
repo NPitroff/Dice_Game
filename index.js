56c98ca7e6f2d0654cc52c1afeab5bb768b391f5
NPitroff <nicolai.pitroff@gmail.com>
2022-01-27 06:58:41 -0800

    Added ID's and variables to select the different dice image

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-randomNumber1 = Math.floor(Math.random()*7);
+// Variable to select player 1 dice
+var playerOne = document.getElementById("playerOne");
+// variable to select player 2 dice
+var playerTwo = document.getElementById("playerTwo");
+
+
+
+randomNumber1 = Math.floor(Math.random()*6)+1;
 console.log(randomNumber1);
