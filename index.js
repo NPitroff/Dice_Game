33ef61305e220e5b7230964f2a7de8c9604ff2a7
NPitroff <nicolai.pitroff@gmail.com>
2022-01-27 07:22:33 -0800

    Added else if statements to change the image on 1-6

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ var playerTwo = document.getElementsByClassName("img2");
 
 
 
-//randomNumber1 = Math.floor(Math.random()*6)+1;
+randomNumber1 = Math.floor(Math.random()*6)+1;
 // loop for player 1 dice to switch the img src
-randomNumber1 = 1;
+// randomNumber1 = 1;
 console.log(randomNumber1);
 
 if(randomNumber1 === 1){
   playerOne[0].setAttribute("src", "images/dice1.png");
+} else if(randomNumber1 === 2){
+  playerOne[0].setAttribute("src", "images/dice2.png");
+}else if(randomNumber1 === 3){
+  playerOne[0].setAttribute("src", "images/dice3.png");
+}else if(randomNumber1 === 4){
+  playerOne[0].setAttribute("src", "images/dice4.png");
+}else if(randomNumber1 === 5){
+  playerOne[0].setAttribute("src", "images/dice5.png");
+}else if(randomNumber1 === 6){
+  playerOne[0].setAttribute("src", "images/dice6.png");
 }
